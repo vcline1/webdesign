e2161ac4a92063ea2a0210cd6d2255d089f70e3d
vcline1 <46933896+vcline1@users.noreply.github.com>
2019-04-16 08:50:34 -0800

    p5 layout

--- e17/js/number.js
+++ e17/js/number.js
@@ -1,34 +1,7 @@
- // $(document).ready(function() {
-    // var number = Math.floor(Math.random() * 1000001);
-
-  	// console.log(number);
-
-  	// var guesses = '';
-
-  	// $('#submit').click(function() {
-  	 	// var guess = $('input').val();
-  	 	// guesses += guess + ' ';
-  	 	// $('.guesses p').text(guesses);
-  		// console.log(guess);
-
-//   		if (number == guess) {
-//   			$('h1').text(number + ' is right!');
-//   			$('input').val('');
-//   		} else if (number < guess) {
-//   			$('h1').text(guess + ' is too high');
-//   			$('input').val('')
-//   		} else {
-//   			$('h1').text(guess + ' is too low');
-//   			$('input').val('');
-//   		}
-//   	});
-// });
-
-
 $(document).ready(function() {
-//   $('.button').click(function() {
-//       $('.button').toggleClass('tomato');
-//   });
+  $('.button').hover(function() {
+      $('.button').toggleClass('tomato');
+  });
 
 	var fruitArray = ["Orange", "Apple", "Watermelon", "Lemon", "Mango", "Grape", "Cherry", "Banana", "Olive"];
 	var randomNumber = Math.floor(Math.random() * fruitArray.length);
