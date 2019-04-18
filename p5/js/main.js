905c203a5c5070b84360bdfa8d3f9e8e4db25c40
vcline1 <46933896+vcline1@users.noreply.github.com>
2019-04-18 09:06:31 -0800

    p4 update

--- p5/js/main.js
+++ p5/js/main.js
@@ -7,6 +7,40 @@ $(document).ready(function() {
         $('.button').toggleClass('gray');
       });
 
-      var coffeeArray = [];
+      var coffeeArray = [ ".coffee1", ".coffee2", ".coffee3", ".coffee4", ".coffee5", ".coffee6", ".coffee7", ".coffee8", ".coffee9" ];
     	var randomNumber = Math.floor(Math.random() * coffeeArray.length);
+
+
+      console.log(coffeeArray[randomNumber]);
+
+      $('.button').click(function() {
+        $('.coffeeimg').css('display', 'block');
+
+      });
 });
+
+
+
+
+//
+//
+// $('#submit').click(function() {
+//
+//   if (guess == stripped || guess == fruitArray[randomNumber]) {
+//     $('h1').text('Yup, ' + guess + " is right.");
+//     $('input').text(guess);
+//     $('.button p').text('Play Again?');
+//     $('#submit').click(function() {
+//       location.reload(true);
+//     });
+//
+//   } else if (guess == none){
+//     $('h1').text('Guess something.....');
+//     $('.button p').text('Guess');
+// } else {
+//     $('h1').text('Nope, not ' + guess);
+//     $('input').val('');
+//     $('.button p').text('Guess Again');
+//   }
+// });
+// });
