c8175111202e4398dcb012e392fb561a598418e9
vcline1 <46933896+vcline1@users.noreply.github.com>
2019-05-01 11:02:38 -0800

    p4 update

---
+++ p4/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $(".content1").slideToggle();
+  });
+
+  $(".btn2").click(function(){
+    $(".content2").slideToggle();
+  });
+
+  $(".btn3").click(function(){
+    $(".content3").slideToggle();
+  });
+
+  $(".btn4").click(function(){
+    $(".content4").slideToggle();
+  });
+});
